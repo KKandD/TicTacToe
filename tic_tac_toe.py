ad34d828ea0b2fb8332028ea61bc168dec606e44
KKandD <filip.brzezinski102@gmail.com>
2020-10-22 19:07:30 +0200

    Implementacja get_move

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,12 +1,33 @@
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
-    board = []
+    board = [['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.']]
     return board
 
 
 def get_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
+    player_move = input()
     row, col = 0, 0
+
+    if player_move == "A1":
+        row, col = 1, 1
+    if player_move == "A2":
+        row, col = 1, 2
+    if player_move == "A3":
+        row, col = 1, 3
+    if player_move == "B1":
+        row, col = 2, 1
+    if player_move == "B2":
+        row, col = 2, 2
+    if player_move == "B3":
+        row, col = 2, 3
+    if player_move == "C1":
+        row, col = 3, 1
+    if player_move == "C2":
+        row, col = 3, 2
+    if player_move == "C3":
+        row, col = 3, 3
+
     return row, col
 
 
@@ -18,6 +39,8 @@ def get_ai_move(board, player):
 
 def mark(board, player, row, col):
     """Marks the element at row & col on the board for player."""
+    if row == 1 and col == 1:
+        pass
     pass
 
 
@@ -33,7 +56,13 @@ def is_full(board):
 
 def print_board(board):
     """Prints a 3-by-3 board on the screen with borders."""
-    pass
+    print(" 1     2     3")
+    print(str(board[0]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
+    print("----+-----+----")
+    print(str(board[1]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
+    print("----+-----+----")
+    print(str(board[2]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
+    
 
 
 def print_result(winner):
