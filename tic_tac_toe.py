a0dce44b26ea1531467607e33ae2914e4c8b360c
Boner1975 <boneckipawel1975@gmail.com>
2020-10-26 19:10:31 +0100

    Fill in body of functions mark, has_won and is_full. Remove comment.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,8 @@
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
+
     board = [['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.']]
     return board
-
 
 def get_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
@@ -11,7 +11,7 @@ def get_move(board, player):
     correct_coordinates = False
 
     while not correct_coordinates:
-        
+    
         player_move = input("Give your coordinates: ")
 
         if player_move == "A1" or player_move == "a1" and board[0][0] == ".":
@@ -45,12 +45,11 @@ def get_move(board, player):
             continue
 
         correct_coordinates = True
-        
-    return(row, col)
-
+    return row, col
 
 def get_ai_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
+
     row, col = 0, 0
     return row, col
 
@@ -58,8 +57,6 @@ def get_ai_move(board, player):
 def mark(board, player, row, col):
     """Marks the element at row & col on the board for player."""
 
-    player = "X"
-    
     if row == 1 and col == 1:
         board[0][0] = player
     elif row == 1 and col == 2:
@@ -77,27 +74,49 @@ def mark(board, player, row, col):
     elif row == 3 and col == 2:
         board[2][2] = player
     elif row == 3 and col == 3:
-        board[2][4] = player
-    
-    if player == "X":
-        player = "0"
-    else:
-        player = "X"
-    
-    return board
+        board[2][4] = player   
 
 
 def has_won(board, player):
     """Returns True if player has won the game."""
-    return False
 
+    is_win = False
+
+    if board[0][0] == player and board[0][2] == player and board[0][4] == player:
+        is_win == True
+    if board[1][0] == player and board[1][2] == player and board[1][4] == player:
+        is_win == True
+    if board[2][0] == player and board[2][2] == player and board[2][4] == player:
+        is_win == True
+    if board[0][0] == player and board[1][0] == player and board[2][0] == player:
+        is_win == True
+    if board[0][2] == player and board[1][2] == player and board[2][2] == player:
+        is_win == True
+    if board[0][4] == player and board[1][4] == player and board[2][4] == player:
+        is_win == True  
+    if board[0][0] == player and board[1][2] == player and board[2][4] == player:
+        is_win == True    
+    if board[2][0] == player and board[1][2] == player and board[0][4] == player:
+        is_win == True
+    
+
+    if is_win:
+        return True
+    else:
+        return False
+    
 
 def is_full(board):
     """Returns True if board is full."""
-    return False
+
+    if not "." in board:
+        return True
+    else:        
+        return False
 
 
 def print_board(board):
+
     """Prints a 3-by-3 board on the screen with borders."""
     print("   1     2     3")
     print("A " + str(board[0]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
@@ -105,7 +124,6 @@ def print_board(board):
     print("B " + str(board[1]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
     print("  ----+-----+----")
     print("C " + str(board[2]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
-    
 
 
 def print_result(winner):
@@ -118,8 +136,8 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
 
     # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
     print_board(board)
-    row, col = get_move(board, 1)
-    mark(board, 1, row, col)
+    row, col = get_move(board, "X")
+    mark(board,"X", row, col)
 
     winner = 0
     print_result(winner)
