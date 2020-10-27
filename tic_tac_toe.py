71fe146920148f127bd0c3432d114341baa0e72f
KKandD <filip.brzezinski102@gmail.com>
2020-10-27 19:34:56 +0100

    Working game. is_full OK, has_won OK

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,10 @@
+import os
+
+
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
 
-    board = [['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.'],['.', ' | ', '.', ' | ', '.']]
+    board = [['.','.','.'],['.','.','.'],['.','.','.']]
     return board
 
 def get_move(board, player):
@@ -16,31 +19,22 @@ def get_move(board, player):
 
         if player_move == "A1" or player_move == "a1" and board[0][0] == ".":
             row, col = (1, 1)
-
-        elif player_move == "A2" or player_move == "a2" and board[0][2] == ".":
+        elif player_move == "A2" or player_move == "a2" and board[0][1] == ".":
             row, col = (1, 2)
-
-        elif player_move == "A3" or player_move == "a3" and board[0][4] == ".":
+        elif player_move == "A3" or player_move == "a3" and board[0][2] == ".":
             row, col = (1, 3)
-
         elif player_move == "B1" or player_move == "b1" and board[1][0] == ".":
             row, col = (2, 1)
-
-        elif player_move == "B2" or player_move == "b2" and board[1][2] == ".":
+        elif player_move == "B2" or player_move == "b2" and board[1][1] == ".":
             row, col = (2, 2)
-
-        elif player_move == "B3" or player_move == "b3" and board[1][4] == ".":
+        elif player_move == "B3" or player_move == "b3" and board[1][2] == ".":
             row, col = (2, 3)
-
         elif player_move == "C1" or player_move == "c1" and board[2][0] == ".":
             row, col = (3, 1)
-
-        elif player_move == "C2" or player_move == "c2" and board[2][2] == ".":
+        elif player_move == "C2" or player_move == "c2" and board[2][1] == ".":
             row, col = (3, 2)
-
-        elif player_move == "C3" or player_move == "c3" and board[2][4] == ".":
+        elif player_move == "C3" or player_move == "c3" and board[2][2] == ".":
             row, col = (3, 3)
-            
         else:
             continue
 
@@ -60,87 +54,125 @@ def mark(board, player, row, col):
     if row == 1 and col == 1:
         board[0][0] = player
     elif row == 1 and col == 2:
-        board[0][2] = player
+        board[0][1] = player
     elif row == 1 and col == 3:
-        board[0][4] = player
+        board[0][2] = player
     elif row == 2 and col == 1:
         board[1][0] = player
     elif row == 2 and col == 2:
-        board[1][2] = player
+        board[1][1] = player
     elif row == 2 and col == 3:
-        board[1][4] = player
+        board[1][2] = player
     elif row == 3 and col == 1:
         board[2][0] = player
     elif row == 3 and col == 2:
-        board[2][2] = player
+        board[2][1] = player
     elif row == 3 and col == 3:
-        board[2][4] = player   
+        board[2][2] = player   
+
+    return mark
 
 
 def has_won(board, player):
     """Returns True if player has won the game."""
 
-    is_win = False
+    player1 = "X"
+    player2 = "0"
+    has_won = False
 
-    if board[0][0] == player and board[0][2] == player and board[0][4] == player:
-        is_win == True
-    if board[1][0] == player and board[1][2] == player and board[1][4] == player:
-        is_win == True
-    if board[2][0] == player and board[2][2] == player and board[2][4] == player:
-        is_win == True
-    if board[0][0] == player and board[1][0] == player and board[2][0] == player:
-        is_win == True
-    if board[0][2] == player and board[1][2] == player and board[2][2] == player:
-        is_win == True
-    if board[0][4] == player and board[1][4] == player and board[2][4] == player:
-        is_win == True  
-    if board[0][0] == player and board[1][2] == player and board[2][4] == player:
-        is_win == True    
-    if board[2][0] == player and board[1][2] == player and board[0][4] == player:
-        is_win == True
+    if board[0][0] == board[0][1] == board[0][2] == player1 or \
+        board[1][0] == board[1][1] == board[1][2] == player1 or \
+        board[2][0] == board[2][1] == board[2][2] == player1 or \
+        board[0][0] == board[1][0] == board[2][0] == player1 or \
+        board[0][1] == board[1][1] == board[2][1] == player1 or \
+        board[0][2] == board[1][2] == board[2][2] == player1 or \
+        board[0][0] == board[1][1] == board[2][2] == player1 or \
+        board[2][0] == board[1][1] == board[0][2] == player1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print_board(board)
+        has_won = True
+    elif board[0][0] == board[0][1] == board[0][2] == player2 or \
+        board[1][0] == board[1][1] == board[1][2] == player2 or \
+        board[2][0] == board[2][1] == board[2][2] == player2 or \
+        board[0][0] == board[1][0] == board[2][0] == player2 or \
+        board[0][1] == board[1][1] == board[2][1] == player2 or \
+        board[0][2] == board[1][2] == board[2][2] == player2 or \
+        board[0][0] == board[1][1] == board[2][2] == player2 or \
+        board[2][0] == board[1][1] == board[0][2] == player2:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print_board(board)
+        has_won = True
+
     
+    return has_won
+   
 
-    if is_win:
-        return True
-    else:
-        return False
     
 
 def is_full(board):
     """Returns True if board is full."""
+    is_full = False
+    
+    if not '.' in board[0] and not '.' in board[1] and not '.' in board[2]:
+        is_full = True 
 
-    if not "." in board:
-        return True
-    else:        
-        return False
-
+    return is_full
 
 def print_board(board):
 
     """Prints a 3-by-3 board on the screen with borders."""
-    print("   1     2     3")
-    print("A " + str(board[0]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
-    print("  ----+-----+----")
-    print("B " + str(board[1]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
-    print("  ----+-----+----")
-    print("C " + str(board[2]).replace("[", " ").replace("]", "").replace("'", "").replace(",","" ))
+    print("  1   2   3")
+    print(f'A {board[0][0]} | {board[0][1]} | {board[0][2]}')
+    print(" ---+---+---")
+    print(f'B {board[1][0]} | {board[1][1]} | {board[1][2]}')
+    print(" ---+---+---")
+    print(f'C {board[2][0]} | {board[2][1]} | {board[2][2]}')
 
 
 def print_result(winner):
     """Congratulates winner or proclaims tie (if winner equals zero)."""
-    pass
+    if winner != 1:
+        print(f"Congrats {winner} is the champion!")
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("It's a tie")
+    
 
 
 def tictactoe_game(mode='HUMAN-HUMAN'):
+
     board = init_board()
+    player = "0"
+    
+    playing = True
+    while playing:
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-    print_board(board)
-    row, col = get_move(board, "X")
-    mark(board,"X", row, col)
 
-    winner = 0
-    print_result(winner)
+        if player == "0":
+            player = "X"
+        else:
+            player = "0"
+        
+        # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
+        print_board(board)
+        row, col = get_move(board, player)
+        mark(board, player, row, col)
+
+        if has_won(board, player) == True:
+            winner = player
+            playing = False
+            print_result(winner)
+
+        if is_full(board) == True:
+            winner = 1
+            playing = False
+            print_result(winner)
+
+    
+
+
+        
 
 
 def main_menu():
